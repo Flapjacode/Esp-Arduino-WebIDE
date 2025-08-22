58fcb18c0e90dbf96c52024b17c55c4b0cc50e3b
flapjacode <flapjack_finance@proton.me>
2025-08-22 00:57:07 +0000

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // ← make sure this is here
+import ArduinoIDE from "./ArduinoIDE.jsx"; // ← use your existing component
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ArduinoIDE />
   </React.StrictMode>
 );
